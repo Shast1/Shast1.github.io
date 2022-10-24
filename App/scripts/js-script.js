554bf1fc6c00743e59f4eb860ffc9c16ee7c955b
Shast1 <116583668+Shast1@users.noreply.github.com>
2022-10-24 21:40:21 +0500

    Add files via upload

---
+++ App/scripts/js-script.js
@@ -0,0 +1,7 @@
+function superClick(){
+		alert('Вся подробная инфа находится в кнопках ')
+	}
+function changeDisplay(){
+	const header3 = document.getElementById("1");
+	header3.style.background='purple';
+}
